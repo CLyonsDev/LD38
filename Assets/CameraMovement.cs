b586053e23e943197597db38201bb79894c1dc41
Collin Lyons <collinclyons@gmail.com>
2017-04-22 00:06:17 -0700

    Day 1
    Lots of stuff, should have probably kept track better!

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    Transform player;
+    float lerpSpeed = 3f;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(player.position.x, player.position.y, transform.position.z), lerpSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/DestroyAfterDelay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterDelay : MonoBehaviour {
+
+    public float delay;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(DestroyThisPlease());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private IEnumerator DestroyThisPlease()
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(this.gameObject);
+    }
+}
